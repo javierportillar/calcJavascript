a844dff20e601f0570d77c0d15804d1970d80f69
piandas <santiagobustosp@gmail.com>
2023-09-12 12:26:06 -0500

    Antes de probar

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-async function operacion (){
+const numberButtons = document.querySelectorAll(".numbers button")
+console.log(numberButtons)
 
 
-
+const numberSelected = (event) => {
+    const numero = event.taget.textContent;
 }
+
+numberButtons.forEach
+
+console.log("numberSelected")
+
 
