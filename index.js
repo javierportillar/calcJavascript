1a5008b7619a3e028ab3f8dd3a72ede2913162e8
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-15 11:15:01 -0500

    Se añidio la opcion de sumar y restar un numero de más de 1 digito

--- index.js
+++ index.js
@@ -23,28 +23,53 @@ let verOpera = document.getElementById("operation-content");
 let numAnterior = "";
 let numActual = "";
 let operador = "";
+let resultadoOp;
 
 // Eventos para recepcion del numero a operar
-cero.addEventListener('click',()=>asignarNumero(0));
-uno.addEventListener('click',()=>asignarNumero(1));
-dos.addEventListener('click',()=>asignarNumero(2));
-tres.addEventListener('click',()=>asignarNumero(3));
-cuac.addEventListener('click',()=>asignarNumero(4));
-cinc.addEventListener('click',()=>asignarNumero(5));
-seis.addEventListener('click',()=>asignarNumero(6));
-siet.addEventListener('click',()=>asignarNumero(7));
-ocho.addEventListener('click',()=>asignarNumero(8));
-nuev.addEventListener('click',()=>asignarNumero(9));
+cero.addEventListener("click", () => asignarNumero(0));
+uno.addEventListener("click", () => asignarNumero(1));
+dos.addEventListener("click", () => asignarNumero(2));
+tres.addEventListener("click", () => asignarNumero(3));
+cuac.addEventListener("click", () => asignarNumero(4));
+cinc.addEventListener("click", () => asignarNumero(5));
+seis.addEventListener("click", () => asignarNumero(6));
+siet.addEventListener("click", () => asignarNumero(7));
+ocho.addEventListener("click", () => asignarNumero(8));
+nuev.addEventListener("click", () => asignarNumero(9));
 
 // Eventos para recepcion del operador.
-operators.addEventListener('click',asignarOperador);
+operators.addEventListener("click", asignarOperador);
 
-function asignarOperador(event){
+// Eventos para acciones
+igualButton.addEventListener("click", () => realizarOp());
+
+//Funciones de control
+function realizarOp() {
+  if (numAnterior != "" && numActual != "") {
+    switch (operador) {
+      case "+":
+        resultadoOp = parseFloat(numActual) + parseFloat(numAnterior);
+        break;
+
+      case "-":
+        resultadoOp = parseFloat(numAnterior) - parseFloat(numActual);
+        break;
+    }
+    console.log(numAnterior, numActual);
+    numActual = resultadoOp;
+    console.log(resultadoOp);
+  }
+}
+function asignarOperador(event) {
   operador = event.target.textContent;
+  numAnterior = numActual;
+  numActual = "";
   console.log(operador);
 }
-function asignarNumero(num){
-  numActual = num;
+function asignarNumero(num) {
+  // Concatenar el número al número actual como una cadena
+  numActual = numActual.toString() + num.toString();
+  // Actualizar la visualización en la pantalla
+  resultado.textContent = numActual;
   console.log(numActual);
 }
-
