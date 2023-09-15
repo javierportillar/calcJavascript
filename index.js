0f170366a09716c7f914d8cb1db24e846b1cf14b
Javier Portilla Rosero <javierportillarosero@MacBook-Pro-de-Javier.local>
2023-09-15 09:07:53 -0500

    Se declararon funciones para recepcion del numero y operador a la logica

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Asignación de elementos HTML a variables JS
+// Asignación de elementos HTML a variables JS //
 const numbers = document.querySelector(".numbers");
 const operators = document.querySelector(".operators");
 const cero = document.querySelector("#cero");
@@ -18,3 +18,33 @@ const datosShow = document.getElementById("datosShow");
 const datosData = document.getElementById("datosData");
 let resultado = document.querySelector("#text-content");
 let verOpera = document.getElementById("operation-content");
+
+// Declaración de variables de control //
+let numAnterior = "";
+let numActual = "";
+let operador = "";
+
+// Eventos para recepcion del numero a operar
+cero.addEventListener('click',()=>asignarNumero(0));
+uno.addEventListener('click',()=>asignarNumero(1));
+dos.addEventListener('click',()=>asignarNumero(2));
+tres.addEventListener('click',()=>asignarNumero(3));
+cuac.addEventListener('click',()=>asignarNumero(4));
+cinc.addEventListener('click',()=>asignarNumero(5));
+seis.addEventListener('click',()=>asignarNumero(6));
+siet.addEventListener('click',()=>asignarNumero(7));
+ocho.addEventListener('click',()=>asignarNumero(8));
+nuev.addEventListener('click',()=>asignarNumero(9));
+
+// Eventos para recepcion del operador.
+operators.addEventListener('click',asignarOperador);
+
+function asignarOperador(event){
+  operador = event.target.textContent;
+  console.log(operador);
+}
+function asignarNumero(num){
+  numActual = num;
+  console.log(numActual);
+}
+
