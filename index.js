d11347a2e8ce25f31c9f2f4a4c6c6f0edd44ed9c
piandas <santiagobustosp@gmail.com>
2023-09-18 09:22:12 -0500

    Agregada rama y realizado cambios calcV2

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-const numberButtons = document.querySelectorAll(".numbers button")
-console.log(numberButtons)
+// Obtener los elementos del HTML
+const inputResult = document.getElementById("result");
+const numberButtons = document.querySelectorAll(".numbers button");
+const operatorButtons = document.querySelectorAll(".operators button");
 
+let = currentOperation = "";
 
+/// FUNCIONES ///
+
+//Funcion para manejar el numero seleccionado en los botones
 const numberSelected = (event) => {
-    const numero = event.taget.textContent;
+    const number = event.target.textContent;
+    currentOperation += number;
+    inputResult.textContent = currentOperation;
 }
 
-numberButtons.forEach
+//Funcion para manejar los operadores con los botones
+const operatorSelected = (event) => {
+    const operator = event.target.textContent;
+    currentOperation += operator
 
-console.log("numberSelected")
+}
 
+//Funciona para hacer el calculo
+const calculate = () => {
+    try {
+        const result = eval(currentOperation);
+        inputResult.textContent = result;
 
+    } catch(error) {
+        inputResult.textContent = "Syntax Error";
+    }
+}
+
+//Funcion para reiniciar la calculadora
+const resetOperation = () => {
+    currentOperation = "";
+    inputResult.textContent = "0";
+}
+
+/// LISTENERS ///
+
+// Event Listeners a los botones de numeros
+numberButtons.forEach((button) => {
+    button.addEventListener("click",numberSelected)
+})
+
+// Event Listeners a los botones de operaciones
+operatorButtons.forEach((button)=> {
+    button.addEventListener("click",operatorSelected)
+})
+
+// Agregar funcionaldiad de =
+document.getElementById("equal").addEventListener("click", calculate)
+// Agregar funcionaldiad de reiniciar
+document.getElementById("reset").addEventListener("click", resetOperation)
